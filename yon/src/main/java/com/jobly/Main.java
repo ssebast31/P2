0f698cc349a06a15056247cfc7c58b1af67c4d33
ssebast31 <ssebastianneri@gmail.com>
2022-09-27 22:25:16 -0700

    pom file updated

--- yon/src/main/java/com/jobly/Main.java
+++ yon/src/main/java/com/jobly/Main.java
@@ -1,13 +1,12 @@
 package com.jobly;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 
 import com.jobly.util.DatabaseInitializerUtil;
 import com.jobly.util.DemoDatabaseInitializerUtil;
